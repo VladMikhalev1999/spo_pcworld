c2d1cf4079939653969467eee7038c8b52f8a735
root <root@debian>
2020-11-30 16:48:24 +0500

    lab 6

--- auth.php
+++ auth.php
@@ -5,11 +5,16 @@ function br($str) {
     $db = mysqli_connect('192.168.0.107', 'root', 'root', 'spo');
     $mail = $_REQUEST['mail'];
     $passwd = $_REQUEST['passwd'];
-    $res = mysqli_query($db, "CALL getUserByEmailPassword(" . br($mail) . "," . br($passwd) . ")");
+    $res = mysqli_query($db, "CALL get_salt(" . br($mail) . ")");
+    $salt = mysqli_fetch_array($res, MYSQL_NUM)[0];
+    mysqli_free_result($res);
+    mysqli_close($db);
+    $db = mysqli_connect('192.168.0.107', 'root', 'root', 'spo');
+    $res = mysqli_query($db, "CALL getUserByEmailPassword(" . br($mail) . "," . br(crypt($passwd, $salt)) . ")");
     if (!$res) {
         echo "[Error] " . mysqli_error($db);
     } else {
         $row = mysqli_fetch_array($res, MYSQL_ASSOC);
-        echo "<div style='color: blue;'><p>email:" . $row["email"] . "</p><p>username:" . $row["username"] . "</p><p>login:" . $row["login"] . "</p><p>password:" . $row["passwd"] . "</p><div>";
+        echo "<div style='color: blue;'><p>email:" . $row["email"] . "</p><p>username:" . $row["username"] . "</p><p>login:" . $row["login"] . "</p><p>cryptPasswd:" . $row["passwd"] . "</p><div>";
     }
 ?>
--- reg.php
+++ reg.php
@@ -1,4 +1,18 @@
 <?php
+function _salt() {
+    $i;
+    $salt = '';
+    for ($i = 1; $i <= 2; $i++) {
+        $rnd = (int)(rand(75) + 48);
+        if (($rnd > 57) && ($rnd < 65)) {
+            $rnd = 65;
+        } else if (($rnd > 90) && ($rnd < 97)) {
+            $rnd = 97;
+        }
+        $salt .= chr($rnd);
+    }
+    return $salt;
+}
 function br($str) {
     return "'" . $str . "'";
 }
@@ -9,7 +23,9 @@ function br($str) {
     $passwd = $_REQUEST['passwd'];
     $repasswd = $_REQUEST['rePasswd'];
     if ($passwd == $repasswd) {
-        $res = mysqli_query($db, 'CALL add_user(' . br($mail) . ',' . br($name) . ',' . br($login) . ',' . br($passwd) . ')');
+        $salt = _salt();
+        $cryptPasswd = crypt($passwd, $salt);
+        $res = mysqli_query($db, 'CALL add_user(' . br($mail) . ',' . br($name) . ',' . br($login) . ',' . br($cryptPasswd) . ')');
         if (!$res) {
             echo "[Error] " . mysqli_error($db);
         } else {
